409f3c2c69442c57be956e40a9bc63097eb87145
alex-colvin <alexcolvin@live.com>
2022-06-30 13:22:34 -0400

    Frontend Operational
    feat: mapped userposts on homepage
    feat: added button to publish posts to LinkedIn

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
       <ul>
         <li className="brand">
           <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            <b>React/Django JWT</b>
+            <b>LINDA</b>
           </Link>
         </li>
         <li>
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Component } from "react";
 import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
+import useCustomForm from "../../hooks/useCustomForm";
 
 import axios from "axios";
 
@@ -8,31 +10,49 @@ const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
+  const currentDate = new Date();
+  const formattedDate = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`
   const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  const [posts, setPosts] = useState([]);
+
 
   useEffect(() => {
-    const fetchCars = async () => {
+    const fetchPosts = async () => {
       try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
+        let response = await axios.get("http://127.0.0.1:8000/api/posts/", {
           headers: {
             Authorization: "Bearer " + token,
           },
         });
-        setCars(response.data);
+        setPosts(response.data);
       } catch (error) {
         console.log(error.response.data);
       }
     };
-    fetchCars();
+    fetchPosts();
   }, [token]);
+
+  async function sendPost(id) {
+    let response = await axios.get(`http://127.0.0.1:8000/api/posts/link/${id}/`,{
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    })
+    if(response.status == 200){
+      console.log(`test response: ${response}`)
+      alert('Posted to LinkedIn')
+    }
+  }
+
   return (
     <div className="container">
-      <h1>Home Page for {user.username}!</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
+      <h1>Scheduled Posts for {user.first_name}!</h1>
+      {posts &&
+        posts.map((post) => (
+          <p key={post.id}>
+            <span>Post Text: {post.post}</span>
+            <span>Post Date: {post.date}</span>
+            <button onClick={() => sendPost(post.id)}>Post to linkedin</button>
           </p>
         ))}
     </div>
